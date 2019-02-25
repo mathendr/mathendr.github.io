9a5c47bd6b9a933cff4b117ba1abf79fdf8d4253
mathendr <mhparty16@gmail.com>
2019-02-24 20:32:13 -0600

    Last commit
    Last minute changes before recording

--- uStates.js
+++ uStates.js
@@ -284,10 +284,10 @@ var uStatePaths;
             var color = $(event.target)[0].style.fill;
             var active = true;
             //determines if the color is not white then it is active
-            if (color == "rgb(255, 255, 255)") {
+            if (color == "rgb(0, 0, 0)") {
                 $(event.target)[0].style.fill = colors(text);
             } else {
-                $(event.target)[0].style.fill = "rgb(255, 255, 255)";
+                $(event.target)[0].style.fill = "rgb(0, 0, 0)";
                 active = false;
             }
             //updates the data on the graph based on which state was clicked and if it was already active or not
@@ -301,7 +301,7 @@ var uStatePaths;
                 return d.d;
             })
             .style("fill", function (d) {
-                return data[d.id].color;
+                return data[d.id].color = "rgb(0,0,0)";
             })
             .on("mouseover", mouseOver).on("mouseout", mouseOut).on("click", onClick);
     }
